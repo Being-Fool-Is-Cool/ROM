78d7259ec6ccda194ca4647131208242c224f56b
Amrita Kumari <amrita.a.kumari@oracle.com>
2020-07-12 16:06:16 +0530

    cook dashboard page

--- src/app/pages/cook/cook-dashboard/cook-dashboard.component.ts
+++ src/app/pages/cook/cook-dashboard/cook-dashboard.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+// enum OrderStatus { Pending = 'pending', Preparing = 'preparing', Prepared = 'prepared', Served = 'served'}
+enum OrderStatus { Pending , Preparing, Prepared, Served}
 
 @Component({
   selector: 'app-cook-dashboard',
@@ -7,9 +11,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CookDashboardComponent implements OnInit {
 
-  constructor() { }
-
+  cookDashboardForm: FormGroup;
+  constructor(private fb: FormBuilder) { }
+  public tables: Array<any>;
+  public tableOrderDetails: Array<any>;
+  public allTabledetails: Array<any>;
+  public default_data: Array<any>;
+  orderStatus = OrderStatus;
   ngOnInit() {
+    this.initForm();
+    this.getAllTables();
+    this.getAllTableDetails();
+  }
+
+  initForm() {
+    this.cookDashboardForm = this.fb.group({
+      tableNumber: [null, [Validators.required]]
+    });
+  }
+
+  getAllTables() {
+    this.tables = ['Table1', 'Table2', 'Table3', 'Table3'];
+  }
+
+  getAllTableDetails() {
+    this.allTabledetails = [
+      { tableNo: 'Table 1', orderName: 'Order 1', itemNames: 'Fish Cutlet curry',  amount: 140, status: 'PENDING'},
+      { tableNo: 'Table 5', orderName: 'Order 2', itemNames: 'chicken curry', amount: 140, status: 'PENDING'}
+    ];
+  }
+
+  getDetailsByTableNo() {
+    this.tableOrderDetails = [
+      {}
+    ]
   }
 
 }
