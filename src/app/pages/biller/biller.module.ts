6b316a6055601431dc4da2978307c197b650c0bf
Amrita Kumari <amrita.a.kumari@oracle.com>
2020-07-12 21:17:26 +0530

    biller dashboard complete

--- src/app/pages/biller/biller.module.ts
+++ src/app/pages/biller/biller.module.ts
@@ -1,13 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SelectModule } from 'ng2-select';
+import { SharedModule } from '../../shared/shared.module';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { NgxPaginationModule } from 'ngx-pagination';
+
 import { BillerDashboardComponent } from './biller-dashboard/biller-dashboard.component';
 import { billerRouting } from './biller.routing';
+import { OrderCheckoutComponent } from './order-checkout/order-checkout.component';
 
 @NgModule({
-  declarations: [BillerDashboardComponent],
+  declarations: [BillerDashboardComponent, OrderCheckoutComponent],
   imports: [
     CommonModule,
-    billerRouting
+    SharedModule,
+    SelectModule,
+    billerRouting,
+    ReactiveFormsModule,
+    NgxPaginationModule,
+    FormsModule
   ]
 })
 export class BillerModule { }
--- src/app/pages/biller/biller.routing.ts
+++ src/app/pages/biller/biller.routing.ts
@@ -1,10 +1,15 @@
 import { Routes, RouterModule } from '@angular/router';
 import { BillerDashboardComponent } from './biller-dashboard/biller-dashboard.component';
+import { OrderCheckoutComponent } from './order-checkout/order-checkout.component';
 
 const childRoutes: Routes = [
     {
         path: 'biller-dashboard',
         component: BillerDashboardComponent
+    },
+    {
+        path: 'order-checkout',
+        component: OrderCheckoutComponent
     }
 ];
 
