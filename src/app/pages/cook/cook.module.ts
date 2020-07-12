37eb14265251345604d1d02d8916371e5033dcee
Amrita Kumari <amrita.a.kumari@oracle.com>
2020-07-12 16:06:16 +0530

    cook dashboard page

--- src/app/pages/cook/cook.module.ts
+++ src/app/pages/cook/cook.module.ts
@@ -1,5 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SelectModule } from 'ng2-select';
+import { SharedModule } from '../../shared/shared.module';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { NgxPaginationModule } from 'ngx-pagination';
+
 import { cookRouting } from './cook.routing';
 import { CookDashboardComponent } from './cook-dashboard/cook-dashboard.component';
 
@@ -7,7 +12,12 @@ import { CookDashboardComponent } from './cook-dashboard/cook-dashboard.componen
   declarations: [CookDashboardComponent],
   imports: [
     CommonModule,
-    cookRouting
+    SharedModule,
+    SelectModule,
+    cookRouting,
+    ReactiveFormsModule,
+    NgxPaginationModule,
+    FormsModule
   ]
 })
 export class CookModule { }
