d1173606e21474d0219edd48f46a7575f9f48f1f
Amrita Kumari <amrita.a.kumari@oracle.com>
2020-07-13 23:52:44 +0530

    admin functionality

--- src/app/pages/admin/admin-functionality/admin-functionality.component.ts
+++ src/app/pages/admin/admin-functionality/admin-functionality.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-admin-functionality',
@@ -7,9 +9,148 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminFunctionalityComponent implements OnInit {
 
-  constructor() { }
+  adminFunctionForm: FormGroup;
+  public adminAccess: Array<any>;
+  public category: Array<any>;
+  public tables: Array<any>;
+  constructor(private fb: FormBuilder) { }
+  public items: Array<any> = [
+    
+  ];
+  public value: any = {};
+  public _disabledV: string = '0';
+  public disabled: boolean = false;
+
+  public refreshValue(value: any): void {
+    this.value = value;
+  }
 
   ngOnInit() {
+    this.initForm();
+    this.initApiCall();
   }
+
+  initApiCall() {
+    this.getAdminAccess();
+    this.getAllCategory();
+    this.getAllTable();
+  }
+  initForm() {
+    this.adminFunctionForm = this.fb.group({
+      category: [null],
+      table: [null]
+    });
+  }
+
+  getAdminAccess() {
+    this.adminAccess = [
+    { id: 'addCat', text: 'Add Category', modal: 'addCat' },
+    { id: 'delCat', text: 'Delete Category' },
+    { id: 'addUser', text: 'Add User' },
+    { id: 'delUser', text: 'Delete User' },
+    { id: 'addTab', text: 'Add Table' },
+    { id: 'delTab', text: 'Delete Table' },
+    { id: 'addItem', text: 'Add Item' },
+    { id: 'delItem', text: 'Delete Item' }
+    ];
+  }
+  // Catergory related api start
+  getAllCategory() {
+    this.category = [
+      { id: 1, text: 'BreakFast' },
+      { id: 2, text: 'Lunch' },
+      { id: 3, text: 'Dinner' },
+      { id: 4, text: 'Snacks' },
+      { id: 5, text: 'Bevrages' },
+      { id: 6, text: 'Deserts' }
+      ];
+
+  }
+
+  onAddCategoryClick(category: any) {
+    let formData =this.adminFunctionForm.value;
+    let addReq ={
+      category: formData.table
+    };
+    console.log(addReq);
+    swal({
+      type: 'success',
+      title: 'Success!',
+      text: 'close it!',
+    });
+  }
+
+  onDeleteCategoryClick(category: any) {
+    let formData =this.adminFunctionForm.value;
+    let addReq ={
+      category: formData.category
+    };
+    console.log(addReq);
+    swal({
+      type: 'success',
+      title: 'Success!',
+      text: 'close it!',
+    });
+  }
+  // Catergory related api end
+
+  //Table related api start
+
+  getAllTable() {
+    this.tables = [
+      { id: 1, text: 'Table 1' },
+      { id: 2, text: 'Table 2' },
+      { id: 3, text: 'Table 3' },
+      { id: 4, text: 'Table 4' },
+      { id: 5, text: 'Table 5' },
+      { id: 6, text: 'Table 6' }
+      ];
+
+  }
+
+  onAddTableClick(table: any) {
+    let formData =this.adminFunctionForm.value;
+    let addReq ={
+      category: formData.table
+    };
+    console.log('Added Table: ', addReq);
+    swal({
+      type: 'success',
+      title: 'Success!',
+      text: 'close it!',
+    });
+  }
+
+  onDeleteTableClick(table: any) {
+    let formData =this.adminFunctionForm.value;
+    let addReq ={
+      category: formData.table
+    };
+    console.log('Deleted Table ',addReq);
+    swal({
+      type: 'success',
+      title: 'Success!',
+      text: 'close it!',
+    });
+  }
+  //Table related api end
+
+  // Modal related call start
+  openModal(modal) {
+    modal.open();
+  }
+
+  closeModal(modal) {
+    modal.close();
+  }
+
+  onSuccess() {
+    swal({
+      type: 'success',
+      title: 'Success!',
+      text: 'close it!',
+    });
+  }
+  // Modal related call end
 
 }
--- src/app/pages/admin/admin.module.ts
+++ src/app/pages/admin/admin.module.ts
@@ -4,6 +4,7 @@ import { SelectModule } from 'ng2-select';
 import { SharedModule } from '../../shared/shared.module';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { NgxPaginationModule } from 'ngx-pagination';
+import { ModalModule } from 'ngx-modal';
 
 import { adminRouting } from './admin.routing';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
@@ -19,7 +20,8 @@ import { AdminFunctionalityComponent } from './admin-functionality/admin-functio
     NgxPaginationModule,
     FormsModule,
     SharedModule,
-    adminRouting
+    adminRouting,
+    ModalModule
   ]
 })
 export class AdminModule { }
