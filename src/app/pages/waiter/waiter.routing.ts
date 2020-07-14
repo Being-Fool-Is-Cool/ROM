db4213e1b42640b49ccea4c94dc4f37ecec129ce
Amrita Kumari <amrita.a.kumari@oracle.com>
2020-07-14 20:05:23 +0530

    take order page

--- src/app/pages/waiter/waiter.routing.ts
+++ src/app/pages/waiter/waiter.routing.ts
@@ -1,11 +1,16 @@
 import { Routes, RouterModule } from '@angular/router';
 import { WaiterDashboardComponent } from './waiter-dashboard/waiter-dashboard.component';
+import { TakeOrderComponent } from './take-order/take-order.component';
 
 
 const childRoutes: Routes = [
     {
         path: 'waiter-dashboard',
         component: WaiterDashboardComponent
+    },
+    {
+        path: 'take-order',
+        component: TakeOrderComponent
     }
 ];
 
